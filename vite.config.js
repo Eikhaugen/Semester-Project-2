a3749f1cb2deb0db762dedb9eabc81a77fc49f45
thomas.eikhaugen@gmail.com <thomas.eikhaugen@gmail.com>
2024-12-02 20:49:49 +0100

    build: add Vite config for MPA build setup

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  appType: "mpa",
+  base: "",
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        login: resolve(__dirname, "./auth/login/index.html"),
+        register: resolve(__dirname, "./auth/register/index.html"),
+        profile: resolve(__dirname, "./profile/index.html"),
+        register: resolve(__dirname, "./listing/index.html"),
+        create: resolve(__dirname, "./create/index.html"),
+      },
+    },
+  },
+});
