f72fd00cbd3efbfa3a326c26b9973e5f72801e71
thomas.eikhaugen@gmail.com <thomas.eikhaugen@gmail.com>
2024-12-04 20:04:40 +0100

    feat: added navbar toggle

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,4 +1,11 @@
 import { setLogoutListener } from '../../ui/auth/logout';
+import { toggleMenu } from '../../ui/toggleMenu';
+
+const navbarToggle = document.getElementById("navbar-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+toggleMenu(navbarToggle, navMenu);
+
 
 document.addEventListener('DOMContentLoaded', () => {
     setLogoutListener();
--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,5 +1,12 @@
 import { setLogoutListener } from '../../ui/auth/logout';
 import { authGuard } from "../../utils/authGuard";
+import { toggleMenu } from '../../ui/toggleMenu';
+
+const navbarToggle = document.getElementById("navbar-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+toggleMenu(navbarToggle, navMenu);
+
 authGuard();
 
 setLogoutListener();
