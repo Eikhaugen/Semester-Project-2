68cc51ff533823acc9708775f22a91550f238a7e
thomas.eikhaugen@gmail.com <thomas.eikhaugen@gmail.com>
2024-12-04 20:41:07 +0100

    feat: add navbar changes if logged in

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,12 +1,54 @@
-import { setLogoutListener } from '../../ui/auth/logout';
+// Import statements
+import { isLoggedIn } from "../../utils/authGuard";
 import { toggleMenu } from '../../ui/toggleMenu';
+import { logout } from '../../ui/auth/logout';
 
+// DOM Elements
 const navbarToggle = document.getElementById("navbar-toggle");
 const navMenu = document.getElementById("nav-menu");
+const logOutBTN = document.getElementById("logout-btn");
 
-toggleMenu(navbarToggle, navMenu);
+// Initialize features and guards
+initializeMenu();
+updateLayout();
+
+// Functions
+
+/**
+ * Initializes the toggle menu functionality.
+ */
+function initializeMenu() {
+    if (navbarToggle && navMenu) {
+        toggleMenu(navbarToggle, navMenu);
+    } else {
+        console.error("Navbar toggle or menu not found in the DOM.");
+    }
+}
+
+/**
+ * Updates the layout based on login status.
+ */
+function updateLayout() {
+    const loggedIn = isLoggedIn();
+
+    const loginBtn = document.getElementById("login-btn");
+    const navMenuBtn = document.getElementById("navbar-toggle")
+
+    if (loggedIn) {
+        loginBtn.classList.add("hidden");
+        loginBtn.classList.remove("block");
+
+        navMenuBtn.classList.add("block");
+        navMenuBtn.classList.remove("hidden");
+    } else {
+        loginBtn.classList.add("block");
+        loginBtn.classList.remove("hidden");
+
+        navMenuBtn.classList.add("hidden");
+        navMenuBtn.classList.remove("block");
+    }
+}
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    setLogoutListener();
-});
+logOutBTN.addEventListener("click", logout)
+
