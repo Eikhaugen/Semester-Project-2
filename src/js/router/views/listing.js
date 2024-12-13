afe418ca4d468ef633ea375f4c2d0b645478e2a8
Thomas Eikhaugen <thomas.eikhaugen@gmail.com>
2024-12-13 19:47:01 +0100

    chore: added/ changed function calls on single listing page

--- src/js/router/views/listing.js
+++ src/js/router/views/listing.js
@@ -1,6 +1,5 @@
 // Import statements
-import { isLoggedIn } from "../../utils/authGuard";
-import { toggleMenu } from '../../ui/toggleMenu';
+import { authGuard, isLoggedIn } from "../../utils/authGuard";
 import { logout } from '../../ui/auth/logout';
 import { renderSingleListing } from "../../ui/listing/renderSingleListing";
 import { fetchSingleListing } from "../../api/listing/read";
@@ -9,15 +8,13 @@ import { getListingIDFromURL } from '../../utils/getListingIDfromURL'
 import { onPlaceBid } from "../../ui/listing/bidOnListing";
 
 // DOM Elements
-const navbarToggle = document.getElementById("navbar-toggle");
-const navMenu = document.getElementById("nav-menu");
 const logOutBTN = document.getElementById("logout-btn");
 const listingContainer = document.getElementById("listing-container");
 const bidsContainer = document.getElementById("listing-bids-container");
 const bidOnListingForm = document.getElementById("bid-on-listing-form");
 
 // Initialize features and guards
-    initializeMenu();
+    authGuard
     updateLayout();
     initializeListings();
     setupSearch();
@@ -26,37 +23,26 @@ const bidOnListingForm = document.getElementById("bid-on-listing-form");
 // Functions
 
 /**
- * Initializes the toggle menu functionality.
- */
-function initializeMenu() {
-    if (navbarToggle && navMenu) {
-        toggleMenu(navbarToggle, navMenu);
-    } else {
-        console.error("Navbar toggle or menu not found in the DOM.");
-    }
-}
-
-/**
  * Updates the layout based on login status.
  */
 function updateLayout() {
     const loggedIn = isLoggedIn();
 
+    const registerBtn = document.getElementById("register-btn");
     const loginBtn = document.getElementById("login-btn");
-    const navMenuBtn = document.getElementById("navbar-toggle");
+    const logOutBtn = document.getElementById("logout-btn");
+    const navMenu = document.getElementById("nav-menu")
 
     if (loggedIn) {
         loginBtn.classList.add("hidden");
-        loginBtn.classList.remove("block");
-
-        navMenuBtn.classList.add("block");
-        navMenuBtn.classList.remove("hidden");
+        registerBtn.classList.add("hidden");
+        logOutBtn.classList.remove("hidden");
+        navMenu.classList.add("block")
+        navMenu.classList.remove("hidden")
     } else {
-        loginBtn.classList.add("block");
         loginBtn.classList.remove("hidden");
-
-        navMenuBtn.classList.add("hidden");
-        navMenuBtn.classList.remove("block");
+        registerBtn.classList.remove("hidden");
+        logOutBtn.classList.add("hidden");
     }
 }
 
