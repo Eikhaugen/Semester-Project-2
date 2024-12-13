80d69084dc3a692bfe0f14a013fdf0518438491c
Thomas Eikhaugen <thomas.eikhaugen@gmail.com>
2024-12-13 19:46:04 +0100

    feat: added function calls and page setup on create listing page

--- src/js/router/views/createListing.js
+++ src/js/router/views/createListing.js
@@ -1,34 +1,19 @@
 // Import statements
-import { isLoggedIn } from "../../utils/authGuard";
-import { toggleMenu } from '../../ui/toggleMenu';
+import { authGuard, isLoggedIn } from "../../utils/authGuard";
 import { logout } from '../../ui/auth/logout';
 import { setupSearch } from "../../ui/listing/searchListing";
-import { onCreateListing } from "../../ui/listing/createListing";
+import { onCreateListing } from "../../ui/listing/createListing"
 
 // DOM Elements
-const navbarToggle = document.getElementById("navbar-toggle");
-const navMenu = document.getElementById("nav-menu");
 const logOutBTN = document.getElementById("logout-btn");
 const createListingForm = document.getElementById("create-listing-form")
 
 // Initialize features and guards
-    initializeMenu();
+    authGuard
     updateLayout();
     setupSearch();
 
-
 // Functions
-
-/**
- * Initializes the toggle menu functionality.
- */
-function initializeMenu() {
-    if (navbarToggle && navMenu) {
-        toggleMenu(navbarToggle, navMenu);
-    } else {
-        console.error("Navbar toggle or menu not found in the DOM.");
-    }
-}
 
 /**
  * Updates the layout based on login status.
@@ -36,21 +21,21 @@ function initializeMenu() {
 function updateLayout() {
     const loggedIn = isLoggedIn();
 
+    const registerBtn = document.getElementById("register-btn");
     const loginBtn = document.getElementById("login-btn");
-    const navMenuBtn = document.getElementById("navbar-toggle");
+    const logOutBtn = document.getElementById("logout-btn");
+    const navMenu = document.getElementById("nav-menu")
 
     if (loggedIn) {
         loginBtn.classList.add("hidden");
-        loginBtn.classList.remove("block");
-
-        navMenuBtn.classList.add("block");
-        navMenuBtn.classList.remove("hidden");
+        registerBtn.classList.add("hidden");
+        logOutBtn.classList.remove("hidden");
+        navMenu.classList.add("block")
+        navMenu.classList.remove("hidden")
     } else {
-        loginBtn.classList.add("block");
         loginBtn.classList.remove("hidden");
-
-        navMenuBtn.classList.add("hidden");
-        navMenuBtn.classList.remove("block");
+        registerBtn.classList.remove("hidden");
+        logOutBtn.classList.add("hidden");
     }
 }
 
